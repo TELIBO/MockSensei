c73e333a315fd1b59295dd12c6ce655d9fc53125
TELIBO <138214709+TELIBO@users.noreply.github.com>
2025-02-01 00:42:36 +0530

    a

--- app/page.js
+++ app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
                 ></path>
               </svg>
             </a>
-            <a
+            {/* <a
               href="https://www.linkedin.com/feed/update/urn:li:activity:7223734705123172353/"
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
@@ -75,7 +75,7 @@ export default function Home() {
                 <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
               </svg>
               Watch video
-            </a>
+            </a> */}
           </div>
           <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
             <span className="font-semibold text-gray-400 uppercase">
